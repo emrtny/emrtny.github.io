464bdbab03b483dbcad353181917ce0f363dce7f
emrtny <emortinpl@gmail.com>
2026-01-28 19:40:58 +0100

    stronka

---
+++ script.js
@@ -0,0 +1,12 @@
+const elements = document.querySelectorAll('.fade-in-element');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.5 });
+
+elements.forEach(el => observer.observe(el));
